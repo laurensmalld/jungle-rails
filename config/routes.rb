09e05f2ae8f6190bd7d9db40a91ef7c7aebe4c4a
laurensmalld <lauren.dentremont@gmail.com>
2016-11-09 22:32:46 +0000

    started user auth

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,12 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
+
+  #routes for registering a new user
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
