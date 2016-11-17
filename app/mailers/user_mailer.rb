e244d4f494b7ea001803949e6cf9fed952d5ef87
laurensmalld <lauren.dentremont@gmail.com>
2016-11-17 00:45:18 +0000

    fixed email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def order_email(order)
     @order = order
     @line_items = order.line_items
-    mail(to: @order.email, subject: "Your Jungle Order: <%= @line_items.order_id %>")
+    mail(to: @order.email, subject: "Your Jungle Order Number #{@order.id}")
   end
 end
